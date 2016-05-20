26b21e6b3d6481aa8e18b16d9630160be84c460e
Vlad Kovtash <vlad@kovtash.com>
2016-05-20 22:54:11 +0300

    Bottom message tracking enhanced

--- VKMessagesViewController/Views/VKTableView.m
+++ VKMessagesViewController/Views/VKTableView.m
@@ -11,18 +11,19 @@
 static CGFloat kInsetCorrectionTollerance = 150;
 static CGFloat kRotationRestorationTollerance = 10;
 
-@interface VKTableView(){
-    CGFloat _newVeticalOffsetStorageFrame;
+@interface VKTableView() {
     NSIndexPath *_lastVisibleRow;
-    BOOL _offsetRestorationRequired;
+    CGRect _previousBounds;
 }
+
 @end
 
 @implementation VKTableView
 
-- (void) setContentInset:(UIEdgeInsets)contentInset{
+- (void)setContentInset:(UIEdgeInsets)contentInset {
     CGFloat bottomOffset = (self.contentSize.height - self.bounds.size.height + self.contentInset.bottom) - self.contentOffset.y;
-    BOOL shouldCorrect = (bottomOffset > kInsetCorrectionTollerance) || (bottomOffset < kInsetCorrectionTollerance && self.contentInset.bottom < contentInset.bottom);
+    BOOL shouldCorrect = (bottomOffset > kInsetCorrectionTollerance) ||
+                         (bottomOffset < kInsetCorrectionTollerance && self.contentInset.bottom < contentInset.bottom);
     
     CGFloat offsetCorrection = 0;
     if (self.contentSize.height < self.bounds.size.height - self.contentInset.top - self.contentInset.bottom) {
@@ -45,67 +46,53 @@ static CGFloat kRotationRestorationTollerance = 10;
     }
 }
 
-- (void) setFrame:(CGRect)frame{
-    if (self.frame.size.width != frame.size.width) {
-        _offsetRestorationRequired = YES;
-        if (self.frame.size.height > self.contentSize.height + self.contentInset.bottom) {
-            _lastVisibleRow = [self indexPathForRowAtPoint:CGPointMake(0, self.contentSize.height + self.contentOffset.y - kRotationRestorationTollerance - self.contentInset.top)];
-        }
-        else{
-            _lastVisibleRow = [self indexPathForRowAtPoint:CGPointMake(0, self.frame.size.height + self.contentOffset.y - kRotationRestorationTollerance - self.contentInset.top)];
-        }
+- (void)setFrame:(CGRect)frame {
+    _previousBounds = self.bounds;
+
+    CGFloat height = CGRectGetHeight(self.bounds);
+
+    if (height > self.contentSize.height + self.contentInset.bottom) {
+        height = self.contentSize.height;
     }
-    else if (self.frame.size.height != frame.size.height) {
-        _offsetRestorationRequired = YES;
-            if (self.frame.size.height > self.contentSize.height) {
-                _newVeticalOffsetStorageFrame = 0;
-            }
-            else{
-                _newVeticalOffsetStorageFrame = self.contentSize.height - self.frame.size.height - self.contentOffset.y;
-            }
-    }
-    else{
-        _offsetRestorationRequired = NO;
-    }
+
+    CGPoint bottomPoint = CGPointMake(0, height + self.contentOffset.y - kRotationRestorationTollerance - self.contentInset.bottom);
+
+    _lastVisibleRow = [self indexPathForRowAtPoint:bottomPoint];
 
     [super setFrame:frame];
-    
-    if (!_offsetRestorationRequired) {
-        return;
-    }
-    
-    if (_lastVisibleRow) {
-        if (_lastVisibleRow.section < [self numberOfSections] && _lastVisibleRow.row < [self numberOfRowsInSection:_lastVisibleRow.section]){
-            [self scrollToRowAtIndexPath:_lastVisibleRow
-                        atScrollPosition:UITableViewScrollPositionBottom animated:NO];
-        }
-        else {
-            [self scrollToBottom];
-        }
-        _lastVisibleRow = nil;
-    }
-    else {
-        if (self.contentSize.height > self.frame.size.height
-            && (self.contentSize.height - self.frame.size.height - _newVeticalOffsetStorageFrame) > -self.contentInset.top) {
-            [self setContentOffset:CGPointMake(self.contentOffset.x,
-                                               self.contentSize.height - self.frame.size.height - _newVeticalOffsetStorageFrame)];
-        }
-        else{
-            [self setContentOffset:CGPointMake(self.contentOffset.x, -self.contentInset.top)];
-        }
-    }
 }
 
-- (void) scrollToBottom{
+- (void)layoutSubviews {
+    [super layoutSubviews];
+
+    if (!_lastVisibleRow || CGRectEqualToRect(_previousBounds, self.bounds)) {
+        return;
+    }
+
+    CGFloat height = CGRectGetHeight(self.bounds);
+
+    if (_lastVisibleRow.section < [self numberOfSections] &&
+        _lastVisibleRow.row < [self numberOfRowsInSection:_lastVisibleRow.section] - 1) {
+        [self scrollToRowAtIndexPath:_lastVisibleRow atScrollPosition:UITableViewScrollPositionBottom animated:NO];
+    }
+    else {
+        [self scrollToBottom];
+    }
+
+    _lastVisibleRow = nil;
+    _previousBounds = CGRectZero;
+}
+
+- (void)scrollToBottom {
     [self scrollToBottomAnimated:NO];
 }
 
-- (void) scrollToBottomAnimated:(BOOL) animated{
-    if (self.contentSize.height + self.contentInset.bottom + self.contentInset.top > self.bounds.size.height){
+- (void)scrollToBottomAnimated:(BOOL)animated {
+    if (self.contentSize.height + self.contentInset.bottom + self.contentInset.top > self.bounds.size.height) {
         [self setContentOffset:CGPointMake(self.contentOffset.x,
                                            self.contentSize.height - self.bounds.size.height + self.contentInset.bottom)
                       animated:animated];
     }
 }
-@end
 
+@end
